82ae45ca591d0554a3dd61369b5ff94e18322723
Victorien <rodriguesvictorien37@gmail.com>
2022-04-11 15:14:26 +0200

    Ajout de la table Listing dans la base de données

--- env/merchex/listings/models.py
+++ env/merchex/listings/models.py
@@ -11,8 +11,20 @@ class Band(models.Model):
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
-    year_formed = models.fields.IntegerField(
-        validators=[MinValueValidator(1900), MaxValueValidator(2022)]
-    )
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2022)])
     active = models.fields.BooleanField(default=True)
     officaial_homepage = models.fields.URLField(null=True, blank=True)
+
+
+class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        RECORDS = 'disques'
+        CLOTHING = 'vêtements'
+        POSTERS = 'affiches'
+        MISCELLANEOUS = 'divers'
+
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2022)], null=True)
+    type = models.fields.CharField(choices=Type.choices, max_length=9)
