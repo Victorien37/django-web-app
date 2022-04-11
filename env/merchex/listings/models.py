1717edd7d5280f601e7cc15d2170c9fedddfdd5a
Victorien <rodriguesvictorien37@gmail.com>
2022-04-11 14:49:22 +0200

    mise à jour de la base de données

--- env/merchex/listings/models.py
+++ env/merchex/listings/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2022)]
+    )
+    active = models.fields.BooleanField(default=True)
+    officaial_homepage = models.fields.URLField(null=True, blank=True)
